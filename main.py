3cd2275c60f598e478bdf585d9acc4be09aa69fd
votmeter <nico.codeluppi.99@gmail.com>
2024-06-12 17:21:37 +0200

    integrazione parte interazioni con db (parte grafica mancante)

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pandas as pd
-from flask import Flask,request,redirect,url_for,render_template
+from flask import Flask,request,jsonify, redirect,url_for,render_template
 from flask_login import LoginManager, current_user, login_user, logout_user, login_required, UserMixin
 from secret import secret_key
 from google.cloud import firestore
 import json
+
+# Funzione per verificare se una collezione esiste
+def collection_exists(collection_name):
+    collections = db.collections()
+    for collection in collections:
+        if collection.id == collection_name:
+            return True
+    return False
+
+# Funzione per verificare se un documento esiste in una collezione
+def document_exists(collection_name, document_name):
+    doc_ref = db.collection(collection_name).document(document_name)
+    doc = doc_ref.get()
+    return doc.exists
 
 
 class User(UserMixin):
@@ -163,6 +177,114 @@ def generate_corridor_plot():
     ref = json.loads(get_references()[0])
 
     return render_template('corridoio_migratorio.html', data=json.dumps(df), ref=json.dumps(ref))
+
+
+# NICO's part
+@app.route('/get_collections', methods=['GET'])
+def get_collections():
+    collections = db.collections()
+    collection_names = [collection.id for collection in collections]
+    return jsonify(collection_names)
+
+@app.route('/get_documents/<collection_name>', methods=['GET'])
+def det_documents(collection_name):
+    documents = db.collection(collection_name).get()
+    document_names = [document.id for document in documents]
+    return jsonify(document_names)
+
+
+# Endpoint per ottenere tutti i documenti di una collezione specifica
+@app.route('/get_documents/<collection_name>', methods=['GET'])
+def get_documents(collection_name):
+    collection_ref = db.collection(collection_name)
+    docs = collection_ref.stream()
+    documents = {doc.id: doc.to_dict() for doc in docs}
+    return jsonify(documents)
+
+@app.route('/collections/<coll>', methods=['GET'])
+def get_all_documents(coll):
+    collection_ref = db.collection(coll)
+    docs = collection_ref.stream()
+    documents = {}
+    for doc in docs:
+        documents[doc.id] = doc.to_dict()
+    return documents
+
+
+# Endpoint per aggiungere una nuova collezione
+@app.route('/add_collection', methods=['POST'])
+def add_collection():
+    data = request.json
+    # collection_id = data.get('id').split(',')
+
+    collection_name = data.get('name')
+    documents = data.get('data')
+    
+    collection_ref = db.collection(collection_name)
+
+    # Recupera tutti i documenti nella collezione
+    docs = list(collection_ref.stream())
+
+    if not docs:
+        # Se la collezione è vuota
+        last_doc_id = 1000000
+    else:
+        # Se ci sono documenti, ordina per ID e prendi l'ultimo
+        docs_sorted = sorted(docs, key=lambda x: x.id)
+        last_doc_id = int(docs_sorted[-1].id)
+        # print(last_doc_id)
+
+
+    # Estrai le coordinate
+    coordinates = documents.get('features')[0].get('geometry').get('coordinates')
+    # print(coordinates)
+    # Converti in lista di tuple (latitudine, longitudine)
+    locoords = [[coord[1], coord[0]] for coord in coordinates]
+        
+    # Loop through each document in the current collection
+    for coord in range(len(locoords)):
+        # Create a reference to the current document in Firestore
+        doc_ref = db.collection(collection_name).document(str(last_doc_id))
+        
+        # Prepare the data to be stored in Firestore
+        x = {
+            "lat": locoords[coord][0],
+            "long": locoords[coord][1]
+        }
+        
+        # Set the data in Firestore for the current document
+        doc_ref.set(x)
+        last_doc_id = last_doc_id + 1
+
+    return 'Collection added', 200
+
+@app.route('/<collection_name>/<document_id>', methods=['GET'])
+def single_document(collection_name, document_id):
+    # Ottieni il riferimento al documento
+    doc_ref = db.collection(collection_name).document(document_id)
+    doc = doc_ref.get()
+
+    # Verifica se il documento esiste
+    if doc.exists:
+        doc_dict = doc.to_dict()
+        return jsonify(doc_dict)
+    else:
+        return jsonify({"error": "Document not found"}), 404
+
+@app.route('/<collection_name>/<document_id>', methods=['POST'])
+def update_document(collection_name, document_id):
+    # Ottieni i dati JSON dalla richiesta
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    # Ottieni il riferimento al documento
+    doc_ref = db.collection(collection_name).document(document_id)
+
+    # Aggiorna il documento
+    doc_ref.set(data, merge=True)
+
+    return jsonify({"success": True}), 200
 
 
 if __name__ == '__main__':
