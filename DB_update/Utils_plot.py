8dc9d351c7c5144502c4a3c723f4e62b4fa7c97c
SimoneMarani <simone.marani10@gmail.com>
2024-06-16 15:42:47 +0200

    commentato graph3

--- DB_update/Utils_plot.py
+++
@@ -1,8 +0,0 @@
-def save_plot(x, y, filename):
-    plt.figure(figsize=(10, 6))
-    plt.plot(x, y, marker='o', linestyle='-', color='b')
-    plt.title('Sequenza di Coordinate')
-    plt.xlabel('Longitudine')
-    plt.ylabel('Latitudine')
-    plt.grid(True)
-    plt.savefig(f'DB_update/images/{filename}.png')
